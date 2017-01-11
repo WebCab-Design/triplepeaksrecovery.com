9ace549a0730239c398cfb7d6549ad881a945825
Alexander Elias <alex.steven.elias@gmail.com>
2017-01-11 09:12:09 -0700

    base

---
+++ src/assets/js/index.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+	var form = document.querySelector('.form');
+	if (form) {
+		Astatine.submit({
+			query: form,
+			method: 'post',
+			responseType: 'json',
+			action: 'https://www.enformed.io/zpn17s0',
+			prepare: function (data, resolve) {
+				data['*default_email'] = 'bburns@peaksrecovery.com';
+				return resolve(data);
+			},
+			complete: function (error, success) {
+				var response = document.querySelector('.response');
+				if (error) {
+					console.log(error);
+					response.style.color = 'red';
+					response.innerText = 'Error Plese See Console';
+				} else {
+					response.style.color = 'green';
+					response.innerText = 'Form Is Submitted';
+				}
+			}
+		});
+	}
+
+});
